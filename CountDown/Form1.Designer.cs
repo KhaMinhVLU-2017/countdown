93c5bc668aa394b3b14fec7910397895cb349d73
KhaMinhVLU-2017 <minhnguyengit@gmail.com>
2017-08-17 16:14:13 +0700

    fixerror

--- CountDown/Form1.Designer.cs
+++ CountDown/Form1.Designer.cs
@@ -28,12 +28,286 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txt_Hour = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txt_Minute = new System.Windows.Forms.TextBox();
+            this.btn_Start = new System.Windows.Forms.Button();
+            this.btn_Pause = new System.Windows.Forms.Button();
+            this.btn_Stop = new System.Windows.Forms.Button();
+            this.label4 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.panel3 = new System.Windows.Forms.Panel();
+            this.panel4 = new System.Windows.Forms.Panel();
+            this.lbl_minute = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.lbl_hour = new System.Windows.Forms.Label();
+            this.lbl_Second = new System.Windows.Forms.Label();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.panel2.SuspendLayout();
+            this.panel3.SuspendLayout();
+            this.panel4.SuspendLayout();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(163)));
+            this.label1.Location = new System.Drawing.Point(5, 3);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(81, 31);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Hour:";
+            // 
+            // txt_Hour
+            // 
+            this.txt_Hour.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(163)));
+            this.txt_Hour.Location = new System.Drawing.Point(92, 3);
+            this.txt_Hour.Multiline = true;
+            this.txt_Hour.Name = "txt_Hour";
+            this.txt_Hour.Size = new System.Drawing.Size(125, 31);
+            this.txt_Hour.TabIndex = 1;
+            this.txt_Hour.KeyDown += new System.Windows.Forms.KeyEventHandler(this.txt_Hour_KeyDown);
+            this.txt_Hour.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.txt_Hour_KeyPress);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(163)));
+            this.label2.Location = new System.Drawing.Point(6, 4);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(103, 31);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Minute:";
+            // 
+            // txt_Minute
+            // 
+            this.txt_Minute.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(163)));
+            this.txt_Minute.Location = new System.Drawing.Point(118, 4);
+            this.txt_Minute.Multiline = true;
+            this.txt_Minute.Name = "txt_Minute";
+            this.txt_Minute.Size = new System.Drawing.Size(125, 31);
+            this.txt_Minute.TabIndex = 3;
+            this.txt_Minute.KeyDown += new System.Windows.Forms.KeyEventHandler(this.txt_Minute_KeyDown);
+            this.txt_Minute.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.txt_Minute_KeyPress);
+            // 
+            // btn_Start
+            // 
+            this.btn_Start.Location = new System.Drawing.Point(4, 22);
+            this.btn_Start.Name = "btn_Start";
+            this.btn_Start.Size = new System.Drawing.Size(87, 41);
+            this.btn_Start.TabIndex = 8;
+            this.btn_Start.Text = "Start";
+            this.btn_Start.UseVisualStyleBackColor = true;
+            this.btn_Start.Click += new System.EventHandler(this.btn_Start_Click);
+            // 
+            // btn_Pause
+            // 
+            this.btn_Pause.Location = new System.Drawing.Point(100, 22);
+            this.btn_Pause.Name = "btn_Pause";
+            this.btn_Pause.Size = new System.Drawing.Size(87, 41);
+            this.btn_Pause.TabIndex = 9;
+            this.btn_Pause.Text = "Pause";
+            this.btn_Pause.UseVisualStyleBackColor = true;
+            this.btn_Pause.Click += new System.EventHandler(this.btn_Pause_Click);
+            // 
+            // btn_Stop
+            // 
+            this.btn_Stop.Location = new System.Drawing.Point(193, 22);
+            this.btn_Stop.Name = "btn_Stop";
+            this.btn_Stop.Size = new System.Drawing.Size(87, 41);
+            this.btn_Stop.TabIndex = 10;
+            this.btn_Stop.Text = "Stop";
+            this.btn_Stop.UseVisualStyleBackColor = true;
+            this.btn_Stop.Click += new System.EventHandler(this.btn_Stop_Click);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 13F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(163)));
+            this.label4.Location = new System.Drawing.Point(365, 40);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(60, 22);
+            this.label4.TabIndex = 13;
+            this.label4.Text = "Zoom:";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(431, 41);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(26, 23);
+            this.button1.TabIndex = 14;
+            this.button1.Text = "+";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(463, 40);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(26, 23);
+            this.button2.TabIndex = 15;
+            this.button2.Text = "-";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // panel2
+            // 
+            this.panel2.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.panel2.Controls.Add(this.button2);
+            this.panel2.Controls.Add(this.btn_Start);
+            this.panel2.Controls.Add(this.button1);
+            this.panel2.Controls.Add(this.btn_Pause);
+            this.panel2.Controls.Add(this.label4);
+            this.panel2.Controls.Add(this.btn_Stop);
+            this.panel2.Location = new System.Drawing.Point(12, 398);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(492, 86);
+            this.panel2.TabIndex = 16;
+            // 
+            // panel3
+            // 
+            this.panel3.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.panel3.Controls.Add(this.label1);
+            this.panel3.Controls.Add(this.txt_Hour);
+            this.panel3.Location = new System.Drawing.Point(12, 3);
+            this.panel3.Name = "panel3";
+            this.panel3.Size = new System.Drawing.Size(220, 40);
+            this.panel3.TabIndex = 17;
+            // 
+            // panel4
+            // 
+            this.panel4.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.panel4.Controls.Add(this.label2);
+            this.panel4.Controls.Add(this.txt_Minute);
+            this.panel4.Location = new System.Drawing.Point(250, 3);
+            this.panel4.Name = "panel4";
+            this.panel4.Size = new System.Drawing.Size(246, 41);
+            this.panel4.TabIndex = 13;
+            // 
+            // lbl_minute
+            // 
+            this.lbl_minute.AutoSize = true;
+            this.lbl_minute.Font = new System.Drawing.Font("Microsoft Sans Serif", 30F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(163)));
+            this.lbl_minute.Location = new System.Drawing.Point(230, 144);
+            this.lbl_minute.Margin = new System.Windows.Forms.Padding(0);
+            this.lbl_minute.Name = "lbl_minute";
+            this.lbl_minute.Size = new System.Drawing.Size(42, 46);
+            this.lbl_minute.TabIndex = 7;
+            this.lbl_minute.Text = "0";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 30F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(163)));
+            this.label5.Location = new System.Drawing.Point(193, 144);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(31, 46);
+            this.label5.TabIndex = 6;
+            this.label5.Text = ":";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 30F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(163)));
+            this.label3.Location = new System.Drawing.Point(278, 144);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(31, 46);
+            this.label3.TabIndex = 11;
+            this.label3.Text = ":";
+            // 
+            // lbl_hour
+            // 
+            this.lbl_hour.AutoSize = true;
+            this.lbl_hour.Font = new System.Drawing.Font("Microsoft Sans Serif", 30F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(163)));
+            this.lbl_hour.Location = new System.Drawing.Point(145, 144);
+            this.lbl_hour.Name = "lbl_hour";
+            this.lbl_hour.Size = new System.Drawing.Size(42, 46);
+            this.lbl_hour.TabIndex = 5;
+            this.lbl_hour.Text = "0";
+            // 
+            // lbl_Second
+            // 
+            this.lbl_Second.AutoSize = true;
+            this.lbl_Second.Font = new System.Drawing.Font("Microsoft Sans Serif", 30F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(163)));
+            this.lbl_Second.Location = new System.Drawing.Point(315, 144);
+            this.lbl_Second.Name = "lbl_Second";
+            this.lbl_Second.Size = new System.Drawing.Size(42, 46);
+            this.lbl_Second.TabIndex = 12;
+            this.lbl_Second.Text = "0";
+            // 
+            // panel1
+            // 
+            this.panel1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.panel1.Controls.Add(this.progressBar1);
+            this.panel1.Controls.Add(this.lbl_hour);
+            this.panel1.Controls.Add(this.lbl_Second);
+            this.panel1.Controls.Add(this.label5);
+            this.panel1.Controls.Add(this.label3);
+            this.panel1.Controls.Add(this.lbl_minute);
+            this.panel1.Location = new System.Drawing.Point(12, 49);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(492, 343);
+            this.panel1.TabIndex = 13;
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Location = new System.Drawing.Point(0, 298);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(492, 45);
+            this.progressBar1.TabIndex = 13;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(516, 492);
+            this.Controls.Add(this.panel4);
+            this.Controls.Add(this.panel3);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.panel1);
+            this.Name = "Form1";
+            this.Text = "CountDown";
+            this.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.Form1_KeyPress);
+            this.panel2.ResumeLayout(false);
+            this.panel2.PerformLayout();
+            this.panel3.ResumeLayout(false);
+            this.panel3.PerformLayout();
+            this.panel4.ResumeLayout(false);
+            this.panel4.PerformLayout();
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txt_Hour;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txt_Minute;
+        private System.Windows.Forms.Button btn_Start;
+        private System.Windows.Forms.Button btn_Pause;
+        private System.Windows.Forms.Button btn_Stop;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Panel panel3;
+        private System.Windows.Forms.Panel panel4;
+        private System.Windows.Forms.Label lbl_minute;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label lbl_hour;
+        private System.Windows.Forms.Label lbl_Second;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.ProgressBar progressBar1;
     }
 }
 
--- CountDown/Form1.cs
+++ CountDown/Form1.cs
@@ -15,6 +15,297 @@ namespace CountDown
         public Form1()
         {
             InitializeComponent();
+            timer1 = new Timer();
+            timer1.Tick += new EventHandler(timer1_MEO);
+            timer1.Interval = 1000;// 1 giay ak
+            btn_Pause.Enabled = false;
+            btn_Stop.Enabled = false;
+            button1.Enabled = true ;
+            button2.Enabled = false;
         }
+        int h, m, s;
+        Timer timer1;
+        private void txt_Hour_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (!Char.IsDigit(e.KeyChar) && !Char.IsControl(e.KeyChar))
+            {
+                e.Handled = true;
+            }
+
+        }
+
+        private void Form1_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (!Char.IsDigit(e.KeyChar) && !Char.IsControl(e.KeyChar))
+            {
+                e.Handled = true;
+            }
+        }
+
+        private void txt_Minute_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (!Char.IsDigit(e.KeyChar) && !Char.IsControl(e.KeyChar))
+            {
+                e.Handled = true;
+            }
+        }
+        int count = 0;
+        int countMinute = 0;
+        int sizeCount = 0;
+        float currentSize;
+        int xMinute = 0;
+        int xHour = 0;
+        int xLabel5=0;
+        int xLabel3=0;
+        int xSecond = 0;
+        int fMinute = 0;
+        int fHour = 0;
+        int fSecond = 0;
+        int fLabel5 = 0;
+        int fLabel3 = 0;
+        private void button1_Click(object sender, EventArgs e)//zoom in
+        {
+            sizeCount++;
+            if (sizeLot != 0)
+            {
+                sizeLot--;
+            }
+            // minute
+            xMinute = 230 - sizeCount * 4;
+            xHour = 145 - sizeCount * 25 - (count - 1) * 20;
+            xSecond = 315 + sizeCount * 10;
+            xLabel3 = 278 + sizeCount * 10;
+            xLabel5 = 193 - sizeCount * 10 + countMinute * 5;
+            fMinute = 20 + sizeCount * 10;
+            fHour = 20 + sizeCount * 10;
+            fSecond = 20 + sizeCount * 10;
+            fLabel5 = 20 + sizeCount * 10;
+            fLabel3 = 20 + sizeCount * 10;
+            if (sizeCount <=4)
+            {
+                checkZoom = true;
+                lbl_minute.Font = new Font("Arial", fMinute);
+                this.lbl_minute.Location = new System.Drawing.Point(xMinute, 144);//center
+                                                                                              //hour
+                lbl_hour.Font = new Font("Arial", fHour);
+                this.lbl_hour.Location = new System.Drawing.Point(xHour, 144);//left
+
+                //second
+                lbl_Second.Font = new Font("Arial", fSecond);
+                this.lbl_Second.Location = new System.Drawing.Point(xSecond, 144);//right
+                //label5
+                this.label5.Location = new System.Drawing.Point(xLabel5, 144);//left
+                label5.Font = new Font("Arial", fLabel5);
+                //label3
+                label3.Font = new Font("Arial", fLabel3);
+                this.label3.Location = new System.Drawing.Point(xLabel3, 144);//phai
+            }
+            else
+            {
+                button1.Enabled = false;
+                button2.Enabled = true;
+            }
+        }
+        string screenh = "";
+
+        private void txt_Hour_KeyDown(object sender, KeyEventArgs e)
+        {
+            screenh = txt_Hour.Text;
+            if (e.KeyCode == Keys.Enter)
+            {
+                btn_Start_Click(null, null);// phim enter
+                txt_Hour.Text = screenh;
+            }
+        }
+
+        private void btn_Start_Click(object sender, EventArgs e)
+        {
+
+            if (txt_Hour.Text == "")
+            {
+                h = 0;
+                txt_Hour.Text = h.ToString();
+            }
+            else
+            {
+                try
+                {
+                    h = int.Parse(txt_Hour.Text);
+                }
+                catch
+                {
+
+                }
+            }
+            if (txt_Minute.Text == "")
+            {
+                m = 0;
+                txt_Minute.Text = m.ToString();
+            }
+            else
+            {
+                try
+                {
+                    m = int.Parse(txt_Minute.Text);
+                }
+                catch
+                {
+
+                }
+            }
+            if (h == 0 && m == 0)
+            {
+                MessageBox.Show("Pease input number in Text");
+            }
+            else
+            {
+                if (m > 60)
+                {
+                    h = m / 60 + h;
+                    m = m % 60;
+                }
+                count = h.ToString().Count();
+                countMinute = m.ToString().Count();
+                if (checkZoom == false)
+                {
+                    lbl_hour.Location = new System.Drawing.Point(145 - (count - 1) * 30, 144);
+                    this.lbl_minute.Location = new System.Drawing.Point(230 - (countMinute - 1) * 12, 144);
+                }
+                progressBar1.Maximum = h*60*60+m*60;
+                timer1.Start();
+                lbl_hour.Text = h.ToString();
+                lbl_minute.Text = m.ToString();
+                enableMeo(false);
+                btn_Pause.Enabled = true;
+                btn_Stop.Enabled = true;
+            }
+
+        }
+        bool checkPause = false;
+        bool checkZoom = false;
+        private void btn_Pause_Click(object sender, EventArgs e)
+        {
+            if (checkPause == false)
+            {
+                timer1.Stop();
+                checkPause = true;
+                btn_Pause.Text = "Resume";
+             
+            }
+            else
+            {
+                btn_Pause.Text = "Pause";
+                timer1.Start();
+                checkPause = false;
+            }
+        }
+
+        private void btn_Stop_Click(object sender, EventArgs e)
+        {
+            timer1.Stop();
+            timer1 = new Timer();
+            timer1.Tick += new EventHandler(timer1_MEO);
+            timer1.Interval = 1000;// 1 giay ak
+            enableMeo(true);
+            h = 0;m = 0;s = 0;
+            lbl_hour.Text = "0";
+            lbl_minute.Text = "0";
+            lbl_Second.Text = "0";
+            btn_Pause.Enabled = false;
+            btn_Stop.Enabled = false;
+            checkPause = false;
+            btn_Pause.Text = "Pause";
+            progressBar1.Value=0;
+        }
+        int sizeLot = 0;
+        private void button2_Click(object sender, EventArgs e)//zomm oiut
+        {
+       
+            sizeLot++;// dem de xoa
+            sizeCount--;
+            // minute
+            if (sizeLot <=4)
+            {
+                checkZoom = true;
+                lbl_minute.Font = new Font("Arial", fMinute - sizeLot * 10);
+                this.lbl_minute.Location = new System.Drawing.Point(xMinute + sizeLot * 4, 144);//center
+                //hour
+                 lbl_hour.Font = new Font("Arial", fHour - sizeLot * 10);
+                 this.lbl_hour.Location = new System.Drawing.Point(xHour + sizeLot * 30 + (count - 2) * 35, 144);//left
+              
+                //second
+                lbl_Second.Font = new Font("Arial", fSecond - sizeLot * 10);
+                this.lbl_Second.Location = new System.Drawing.Point(xSecond - sizeLot * 10, 144);//right
+                  //label5
+                this.label5.Location = new System.Drawing.Point(xLabel5 + sizeLot * 10 + countMinute , 144);//left
+                label5.Font = new Font("Arial", fLabel5 - sizeLot * 10);
+                //label3
+                label3.Font = new Font("Arial", fLabel3 - sizeLot * 10);
+                this.label3.Location = new System.Drawing.Point(xLabel3 - sizeLot * 10, 144);//phai
+            }
+            //xMinute = 230 - sizeCount * 4;
+            //xHour = 145 - sizeCount * 25 - (count - 1) * 20;
+            //xSecond = 315 + sizeCount * 10;
+            //xLabel3 = 278 + sizeCount * 10;
+            //xLabel5 = 193 - sizeCount * 10 + countMinute * 5;
+            //fMinute = 20 + sizeCount * 10;
+            //fHour = 20 + sizeCount * 10;
+            //fSecond = 20 + sizeCount * 10;
+            //fLabel5 = 20 + sizeCount * 10;
+            //fLabel3 = 20 + sizeCount * 10;
+            else
+            {
+                button2.Enabled = false;
+                button1.Enabled = true;
+            }
+        }
+
+        private void txt_Minute_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Enter)
+            {
+
+            }
+        }
+
+        private void timer1_MEO(object sender, EventArgs e)
+        {
+            s--;
+            if (s == -1)
+            {
+                m--;
+                s = 59;
+
+            }
+            if (m == -1)
+            {
+                h--;
+                m = 59;
+            }
+            if (h == 0 && m == 0 && s == 0)
+            {
+                timer1.Stop();
+            }
+            if (h == 0 && m == 59)
+            {
+                this.lbl_minute.Location = new System.Drawing.Point(230 - 10, 144);
+            }
+
+            string hh = Convert.ToString(h);
+            string mm = Convert.ToString(m);
+            string ss = Convert.ToString(s);
+            lbl_hour.Text = hh;
+            lbl_minute.Text = mm;
+            lbl_Second.Text = ss;
+            progressBar1.Increment(1);
+           
+        }
+        private void enableMeo(bool meo)
+        {
+            btn_Start.Enabled = meo;
+            txt_Hour.Enabled = meo;
+            txt_Minute.Enabled = meo;
+        }
+      
     }
 }
